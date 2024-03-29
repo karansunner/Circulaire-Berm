133162dbc9c76aad00a87509c6e3b7f94e5192cf
karansunner <singh_karan100@hotmail.com>
2024-03-29 13:10:25 +0100

    .

--- python2QGIS.py
+++ python2QGIS.py
@@ -5,19 +5,26 @@ DIT RUN JE BINNEN DE PYTHON CONSOLE VAN QGIS
 from qgis.core import QgsVectorLayer, QgsProject
 import os
 
+# hierin zitten alle .shp files
+dir_path = r'C:\Users\singh\Documents\INMINDAEN-Python-Code\db2qgis\data\\'
+
+# voegt alle .shp files toe aan een lijst
+file_paths = []
+for filename in os.listdir(dir_path):
+    if filename.endswith('.shp'):
+        file_path = dir_path + filename
+        file_paths.append(file_path)
+
+# per file maakt die een laag en voegt deze toe aan het project
 def add_layers_from_files(file_paths):
     for path in file_paths:
         layer = QgsVectorLayer(path, os.path.basename(path), 'ogr')
         if layer.isValid():
             QgsProject.instance().addMapLayer(layer)
-            print(f'layer {os.path.basename(path)} added')
+            print(f'Layer {os.path.basename(path)} added successfully.')
         else:
-            print(f'layer {os.path.basename(path)} failed')
-
-# voor nu lokaal, straks willen we refereren naar of cosmos db of de blob storage.
-file_paths = [
-    r'C:\Users\singh\Documents\INMINDAEN-Python-Code\db2qgis\data\Ecologisch_beheer_1e_meter.shp'
-]
+            print(f'Layer {os.path.basename(path)} failed to load!')
 
 add_layers_from_files(file_paths)
+
 iface.mapCanvas().refreshAllLayers()
