2d09756864092998b64f422ad1b171ec63e9e839
karansunner <singh_karan100@hotmail.com>
2024-04-18 15:05:00 +0200

    oplevering

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from shapely.geometry import LineString
 import geopandas as gpd
 from qgis.core import QgsVectorLayer, QgsProject, QgsCoordinateReferenceSystem
 
+# Haalt wegnamen op die als kaartlaag moeten worden toegevoegd
 def read_highway_names(filename):
   highway_names = []
   try:
@@ -13,6 +14,7 @@ def read_highway_names(filename):
     print(f"Error: File '{filename}' not found.")
   return highway_names
 
+# o.b.v. naam van de weg de coordinaten ophalen
 def fetch_way_coordinates(way_name):
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = f"""
@@ -35,56 +37,52 @@ def fetch_way_coordinates(way_name):
     for way in ways:
         way_nodes = way['nodes']
         line_coords = [nodes[node_id] for node_id in way_nodes if node_id in nodes]
-        if line_coords:
-            print(f"Way ID: {way['id']} - Line Coordinates: {line_coords}")  
-            lines.append(LineString(line_coords))
-        else:
-            print(f"Geen coördinaten gevonden voor weg ID: {way['id']}") 
-    print('Coordinaten toegevoegd aan lijst.')
-    return lines
+        lines.append(LineString(line_coords))
 
-def create_shp_file(way_name, shp_output_dir):
-    lines = fetch_way_coordinates(way_name)
-    if lines:
-        print(f"Er zijn {len(lines)} lijnen gevonden.")
-    else:
-        print("Er zijn geen lijnen gevonden voor deze wegnaam.")
-    
-    def create_shp_from_lines(lines, file_path):
-        gdf = gpd.GeoDataFrame(geometry=lines, crs="EPSG:4326")
-        gdf.to_file(file_path, driver='ESRI Shapefile')
+    gdf = gpd.GeoDataFrame(geometry=lines)
+    gdf['wegnaam'] = way_name
+    return gdf
 
-    output_file_path = os.path.join(shp_output_dir, f"{way_name}.shp")
-    create_shp_from_lines(lines, output_file_path)
+# maakt shapefile 
+def save_to_shapefile(gdf, output_path):
+    gdf.to_file(output_path, driver='ESRI Shapefile', crs="EPSG:4326")
+    print("Shapefile saved successfully.")
 
-def gather_all_files(dir_path):
+def gather_all_files(dir_path, processed_files):
     file_paths = []
     for filename in os.listdir(dir_path):
         if filename.endswith('.shp'):
-            file_path = dir_path + filename
-            file_paths.append(file_path)
-    return file_paths
+            file_path = os.path.join(dir_path, filename)
+            if file_path not in processed_files:
+                file_paths.append(file_path)
+                processed_files.append(file_path)
+    return file_paths, processed_files
 
+# voegt shapefile toe als nieuwe laag binnen QGIS 
 def add_layers_from_files(file_paths):
     for path in file_paths:
-        layer = QgsVectorLayer(path, os.path.basename(path), 'ogr')
+        layer_name = os.path.basename(path).split('.')[0]
+        layer = QgsVectorLayer(path, layer_name, 'ogr')
         if layer.isValid():
             layer.setCrs(crs)
             QgsProject.instance().addMapLayer(layer)
-            print(f'Layer {os.path.basename(path)} added successfully.')
+            print(f'Layer {layer_name} added successfully.')
         else:
-            print(f'Layer {os.path.basename(path)} failed to load!')
-
+            print(f'Layer {layer_name} failed to load!')
 
 shp_output_dir = r'C:\Users\singh\Documents\INMINDAEN-Python-Code\db2qgis\data'
 shp_read_dir = r'C:\Users\singh\Documents\INMINDAEN-Python-Code\db2qgis\data\\'
-way_names_file = r'C:\Users\singh\Documents\INMINDAEN-Python-Code\db2qgis\data\highway_names.txt'
+way_names_file = r'C:\Users\singh\Documents\INMINDAEN-Python-Code\db2qgis\highway_names.txt'
 crs = QgsCoordinateReferenceSystem('EPSG:4326')
 
-way_names = read_highway_names(way_names_file)
 
+way_names = read_highway_names(way_names_file) 
+processed_files = [] # houdt bij welke wegen al als laag zijn toegevoegd om duplicatie te voorkomen binnen QGIS 
 for way_name in way_names:
-    create_shp_file(way_name, shp_output_dir)
-    file_paths = gather_all_files(shp_read_dir)
+    output_file_path = os.path.join(shp_output_dir, f"{way_name}.shp")
+    gdf = fetch_way_coordinates(way_name)
+    save_to_shapefile(gdf, output_file_path)
+    file_paths, processed_files = gather_all_files(shp_read_dir, processed_files)
     add_layers_from_files(file_paths)
     iface.mapCanvas().refreshAllLayers()
+    
--- mongo2txt.py
+++ mongo2txt.py
@@ -4,12 +4,11 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["mydb"]
 collection = db["ProjectMinor"]
 
-cursor = collection.find({}, {"Weg": 1})
-values = [doc["Weg"] for doc in cursor]
+cursor = collection.find({}, {"Weg": 1})  
+values = [(doc["Weg"]) for doc in cursor]
 
-with open("data/highway_names.txt", "w", encoding="utf-8") as f:
-    for name in values:
-        f.write(name + "\n")
+with open("data/test.txt", "w", encoding="utf-8") as f:
+    for weg, bloemenaanbod in values:
+        f.write(weg)
 
 client.close()
-
